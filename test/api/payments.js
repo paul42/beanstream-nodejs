cbd45b9de1aae7860d66fd546f97f73ccaac3c07
Brent Owens <bowens@beanstream.com>
2015-12-16 14:56:27 -0800

    updated tests and added getPayment()

--- test/api/payments.js
+++ test/api/payments.js
@@ -35,7 +35,7 @@ describe("api", function() {
 					done();
 				});
 		});
-		it('Should get payment declined', function(done) {
+		it('Should get a declined payment', function(done) {
 			var cardPayment = {
 				order_number: testUtil.getOrderNum("payment"),
 			   	amount:10.00,
@@ -50,7 +50,7 @@ describe("api", function() {
 			};
 			beanstream.payments.makePayment(cardPayment)
 				.then(function(response){
-					expect(false); // should not get here
+					expect(false).to.be.true; // should not get here
 					done();
 				})
 				.catch(function(error){
@@ -63,6 +63,61 @@ describe("api", function() {
 					);
 					done();
 				});
+			
 		});
+		it('Should successfully get a past payment', function(done) {
+			beanstream.payments.getPayment(10122494)
+				.then(function(transaction){
+					expect(transaction).to.not.be.null;
+					expect(transaction).to.have.property('amount', 10);
+					done();
+				})
+				.catch(function(error){
+					expect(error).to.be.null;
+					done();
+				});
+		});
+		it('Should successfully get a new payment', function(done) {
+			var cardPayment = {
+				order_number: testUtil.getOrderNum("payment"),
+			   	amount:10.00,
+			   	payment_method:"card",
+			   	card:{
+			      	name:"John Doe",
+			      	number:"5100000010001004",
+			      	expiry_month:"02",
+			      	expiry_year:"19",
+			      	cvd:"123"
+			   	}
+			};
+			var transactionId;
+			beanstream.payments.makePayment(cardPayment)
+				.then(function(response){
+					expect(response).to.have.property('approved', '1');
+					transactionId = parseInt(response.id, 10);
+					return transactionId;
+				})
+				.then(function(transId){
+					return beanstream.payments.getPayment(transId);
+				})
+				.then(function(transaction) {
+					expect(transaction).to.have.property('id', transactionId);
+					expect(transaction).to.have.property('amount', 10);
+					expect(transaction.card).to.deep.equal({ 
+						name: 'John Doe',
+				     	expiry_month: '02',
+				     	expiry_year: '19',
+				     	card_type: 'MC',
+				     	last_four: '1004',
+				     	avs_result: 'U',
+				     	cvd_result: '1' });
+					done();
+				})
+				.catch(function(error){
+					expect(error.message).to.be.null;
+					done();
+				});
+		});
+		
 	});
 });
