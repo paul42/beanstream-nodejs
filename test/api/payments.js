613dd14939d00ece8f27203ae70e811aa82f88c0
Brent Owens <bowens@beanstream.com>
2015-12-15 16:42:25 -0800

    got new payments working after refactor

--- test/api/payments.js
+++ test/api/payments.js
@@ -12,7 +12,7 @@ describe("testUtil", function() {
 
 describe("api", function() {
 	describe("payments", function() {
-		it('Should successfully make payment', function() {
+		it('Should successfully make payment', function(done) {
 			var cardPayment = {
 				order_number: testUtil.getOrderNum("payment"),
 			   	amount:10.00,
@@ -27,11 +27,41 @@ describe("api", function() {
 			};
 			beanstream.payments.makePayment(cardPayment)
 				.then(function(response){
-					console.log("Got response: ", response);
-					return expect(true);
+					expect(response).to.have.property('approved', '1');
+					done();
 				})
 				.catch(function(error){
-					return expect(error).to.be.null;
+					expect(error).to.be.null;
+					done();
+				});
+		});
+		it('Should get payment declined', function(done) {
+			var cardPayment = {
+				order_number: testUtil.getOrderNum("payment"),
+			   	amount:10.00,
+			   	payment_method:"card",
+			   	card:{
+			      	name:"John Doe",
+			      	number:"1234",
+			      	expiry_month:"02",
+			      	expiry_year:"19",
+			      	cvd:"123"
+			   	}
+			};
+			beanstream.payments.makePayment(cardPayment)
+				.then(function(response){
+					expect(false); // should not get here
+					done();
+				})
+				.catch(function(error){
+					expect(error).to.not.be.null;
+					expect(error).to.have.property('status', 400);
+					expect(error).to.have.property('code', 314);
+					expect(error).to.have.property('category', 3);
+					expect(error.details).to.deep.equal(
+						[ { field: 'card:number', message: 'Invalid Card Number' } ]
+					);
+					done();
 				});
 		});
 	});
